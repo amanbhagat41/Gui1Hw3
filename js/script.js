7412e636aaac7bc9c195bc3e8a6231e393a07722
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2023-11-06 14:07:47 -0500

    Changes

--- js/script.js
+++ js/script.js
@@ -8,29 +8,64 @@ document.querySelector("form").addEventListener("submit", function (e) {
   console.log(maxCol);
   console.log(minRow);
   console.log(maxRow);
-  if (isNaN(minCol) || isNaN(maxCol) || isNaN(minRow) || isNaN(maxRow)) {
-    const errorElement = document.createElement("p");
-    errorElement.innerText = "Incorrect Values";
-    const errorMes = document.getElementById("errMes");
-    errorMes.appendChild(errorElement);
-    return;
-  }
+  const errorElement = document.createElement("p");
+  const errorMes = document.getElementById("errMes");
   const table = document.createElement("table");
   const tabledata = document.getElementById("table");
+  if (isNaN(minCol) || isNaN(maxCol) || isNaN(minRow) || isNaN(maxRow)) {
+    errorMes.innerHTML = "";
+    errorElement.innerText = "Incorrect Values";
+    errorMes.appendChild(errorElement);
+    return;
+  } else if (minRow > maxRow && minCol > maxCol) {
+    errorMes.innerHTML = "";
+    errorElement.innerText =
+      "Minimum Col and Minimum Row can not be bigger than Maximum Col and Maximum Row";
+    errorMes.appendChild(errorElement);
+    return;
+  } else if (minCol > maxCol) {
+    errorMes.innerHTML = "";
+    errorElement.innerText = "Minimum Col can not be bigger than Maximum Col";
+    errorMes.appendChild(errorElement);
+    return;
+  } else if (minRow > maxRow) {
+    errorMes.innerHTML = "";
+    errorElement.innerText = "Minimum Row can not be bigger than Maximum Row";
+    errorMes.appendChild(errorElement);
+    return;
+  } else if (maxCol > 50 || minCol < -50 || maxRow > 50 || minRow < -50) {
+    errorMes.innerHTML = "";
+    errorElement.innerText = "The Range of the table is -50 - 50";
+    errorMes.appendChild(errorElement);
+    return;
+  } else {
+    errorMes.innerHTML = "";
+    errorElement.innerText =
+      "Creating Table, All empty boxes are replaced with 0";
+    errorMes.appendChild(errorElement);
+  }
+
   tabledata.innerHTML = "";
   for (let i = minRow - 1; i <= maxRow; i++) {
     const row = document.createElement("tr");
     for (let j = minCol - 1; j <= maxCol; j++) {
-      const col = document.createElement("td");
-      if (i === minRow - 1 && j === minCol - 1) col.innerText = "";
-      else if (i === minRow - 1) {
-        col.innerText = j;
+      if (i === minRow - 1 && j === minCol - 1) {
+        const colH = document.createElement("th");
+        colH.innerText = "";
+        row.appendChild(colH);
+      } else if (i === minRow - 1) {
+        const colH = document.createElement("th");
+        colH.innerText = j;
+        row.appendChild(colH);
       } else if (j == minCol - 1) {
-        col.innerText = i;
+        const colH = document.createElement("th");
+        colH.innerText = i;
+        row.appendChild(colH);
       } else {
+        const col = document.createElement("td");
         col.innerText = i * j;
+        row.appendChild(col);
       }
-      row.appendChild(col);
     }
     table.appendChild(row);
   }
